135c8a9a688ecea6c1047e25eee6cf2f28511eb0
Alejandra Cernas <stephaniacernas@gmail.com>
2019-09-09 11:24:00 -0500

    Add basic model validations for role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
+# frozen_string_literal: true
 class User < ApplicationRecord
+  has_secure_password
+
+  enum role: { 'admin': 0, 'holder': 1 }
+  # Email validation from ruby docs
+  # https://www.rubydoc.info/stdlib/uri/URI/MailTo
   validates :email, uniqueness: true,
                     presence: true,
                     format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: 8 }
+  validates :role, inclusion: { in: User.roles.keys },
+                   presence: true
+
 end
