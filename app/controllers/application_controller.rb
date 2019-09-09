c70bf088ff9d87d37de9bccea3687668f68a57d1
Alejandra Cernas <stephaniacernas@gmail.com>
2019-09-09 14:11:35 -0500

    Adds filter to check for invalid tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user!
+
+  private
+
+  def authenticate_user!
+
+    return render json: { error: 'token not present' }, status: :unauthorized unless token
+
+    decoded_token = JwtDecoder.decode(token)
+
+    render json: { error: 'invalid token' }, status: :unauthorized unless decoded_token
+  end
+
+  def token
+    @token ||= request.headers['Authorization']&.split(' ')&.last
+  end
 end
