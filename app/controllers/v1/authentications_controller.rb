00b2d43009642e52691f6a1faf7d93db78eff39d
Alejandra Cernas <stephaniacernas@gmail.com>
2019-09-09 14:12:39 -0500

    Save token when user authenticated

--- app/controllers/v1/authentications_controller.rb
+++ app/controllers/v1/authentications_controller.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
-require 'jwt_encoder'
 
 module V1
   class AuthenticationsController < ApplicationController
+    skip_before_action :authenticate_user!
+
     def create
       user = User.find_by(email: user_params[:email])
 
       if user&.authenticate(user_params[:password])
-        render json: { token: JwtEncoder.encode(user.id) }, status: :ok
+        token = JwtEncoder.encode(user.id)
+        if user.update_attribute(:token, token)
+          render json: { token: token }, status: :ok
+        else
+          render json: { error: 'unauthorized' }, status: :unauthorized
+        end
       else
         render json: { error: 'unauthorized' }, status: :unauthorized
       end
