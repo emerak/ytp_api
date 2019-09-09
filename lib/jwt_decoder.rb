65e1ffa34ccfeffb36ab82ad5628cd1ea8e7b827
Alejandra Cernas <stephaniacernas@gmail.com>
2019-09-09 14:20:17 -0500

    Remove true and false values on block

--- lib/jwt_decoder.rb
+++ lib/jwt_decoder.rb
@@ -2,16 +2,16 @@ module JwtDecoder
   def self.decode(token)
     secret = Rails.application.credentials.secret_key_base
 
-    payload = { aud: ENV['CLIENT_ID'],
-                verify_aud: true,
-                iss: ENV['APP_ID'],
-                verify_iss: true,
-                algorithm: 'HS256' }
+    payload = {
+      iss: ENV['APP_ID'],
+      verify_iss: true,
+      algorithm: 'HS256'
+    }
 
     begin
-      [false, JWT.decode(token, secret, true, payload)]
-    rescue  => e
-      [true, e]
+      JWT.decode(token, secret, true, payload)
+    rescue StandardError => e
+      e
     end
   end
 end
