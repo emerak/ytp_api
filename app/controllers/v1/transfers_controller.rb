6792fd156aa18b3e68b6aeb4e7c0c70a5d06bb48
Alejandra Cernas <stephaniacernas@gmail.com>
2019-09-10 17:27:53 -0500

    Update docs

--- app/controllers/v1/transfers_controller.rb
+++ app/controllers/v1/transfers_controller.rb
@@ -2,6 +2,13 @@
 
 module V1
   class TransfersController < ApplicationController
+
+    formats ['json']
+    description "Make a Deposit to an user's account"
+    api :POST, '/v1/transfers'
+    param :destination, String, desc: 'user clabe', required: true
+    param :amount, String, desc: 'transfer amount', required: true
+
     def create
       user = User.find_by(token: @token)
 
