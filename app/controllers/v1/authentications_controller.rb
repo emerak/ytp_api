45daaeb9d539fc596853abfb10c92633e2ba76bc
Alejandra Cernas <stephaniacernas@gmail.com>
2019-09-10 11:16:20 -0500

    Fixes specs and documentation

--- app/controllers/v1/authentications_controller.rb
+++ app/controllers/v1/authentications_controller.rb
@@ -7,18 +7,18 @@ module V1
     formats ['json']
     description 'Authenticates an user by email and password'
     api :POST, '/v1/login'
-    param :user, Hash, desc: 'User info', required: true do
-      param :email, String, desc: 'user email', required: true
-      param :password, String, desc: 'user password', required: true
-    end
+    param :email, String, desc: 'user email', required: true
+    param :password, String, desc: 'user password', required: true
 
     def create
       user = User.find_by(email: user_params[:email])
 
       if user&.authenticate(user_params[:password])
         token = JwtEncoder.encode(user.id)
+
         if user.update_attribute(:token, token)
-          render json: { token: token }, status: :ok
+          user.create_external_account!
+          render json: { token: token, clabe: user.external_account.clabe }, status: :ok
         else
           render json: { error: 'unauthorized' }, status: :unauthorized
         end
@@ -30,7 +30,7 @@ module V1
     private
 
     def user_params
-      params.require(:user).permit(:email, :password)
+      params.permit(:email, :password)
     end
   end
 end
