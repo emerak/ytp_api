d5d0c27535203d6fc03f474e45ad4de66b8f832b
Alejandra Cernas <stephaniacernas@gmail.com>
2019-09-11 14:00:18 -0500

    Suppress deprecation warning on locale

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -2,6 +2,7 @@ MoneyRails.configure do |config|
 
   # set the default currency
   config.default_currency = :mxn
+  config.locale_backend = nil
   config.default_format = {
     symbol: nil,
     no_cents_if_whole: true
